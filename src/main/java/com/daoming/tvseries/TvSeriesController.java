c651cc121799caaefe98af8d2955bc06e53432f6
daoming.lzh <daoming.lzh@alibaba-inc.com>
2018-10-21 19:57:07 +0800

    使用数据结构返回数据

--- src/main/java/com/daoming/tvseries/TvSeriesController.java
+++ src/main/java/com/daoming/tvseries/TvSeriesController.java
@@ -4,17 +4,25 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 @RestController
 @RequestMapping("/tvseries")
 public class TvSeriesController {
 
+//    @GetMapping
+//    public Map<String, Object> sayHello() {
+//        Map<String, Object> map = new HashMap<>();
+//        map.put("message", "hello,world");
+//        return map;
+//    }
+
     @GetMapping
-    public Map<String, Object> sayHello() {
-        Map<String, Object> map = new HashMap<>();
-        map.put("message", "hello,worldsss");
-        return map;
+    public List<TvSeriesDto> getAll() {
+        List<TvSeriesDto> list = new ArrayList<>();
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(2016, Calendar.OCTOBER, 2, 0, 0);
+        list.add(new TvSeriesDto(1, "WestWorld", 1, calendar.getTime()));
+        return list;
     }
 }
