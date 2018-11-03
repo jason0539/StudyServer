29633de65cb3c6e7391ce0504e2e48925fa2087f
jason0539 <jason0539@163.com>
2018-11-03 17:03:40 +0800

    get方式获取指定资源

--- src/main/java/com/daoming/tvseries/TvSeriesController.java
+++ src/main/java/com/daoming/tvseries/TvSeriesController.java
@@ -2,7 +2,9 @@ package com.daoming.tvseries;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,13 +24,27 @@ public class TvSeriesController {
 
     @GetMapping
     public List<TvSeriesDto> getAll() {
-        List<TvSeriesDto> list = new ArrayList<>();
-        list.add(createPoi());
-        list.add(createWestWorld());
         if (log.isDebugEnabled()) {
             log.debug("TvSeriesController.getAll被调用了");
         }
+        List<TvSeriesDto> list = new ArrayList<>();
+        list.add(createPoi());
+        list.add(createWestWorld());
         return list;
+    }
+
+    @GetMapping("/{id}")
+    public TvSeriesDto getOne(@PathVariable int id) {
+        if (log.isDebugEnabled()) {
+            log.debug("getOne " + id);
+        }
+        if (id == 101) {
+            return createPoi();
+        } else if (id == 102) {
+            return createWestWorld();
+        } else {
+            throw new ResourceNotFoundException();
+        }
     }
 
     private TvSeriesDto createPoi() {
