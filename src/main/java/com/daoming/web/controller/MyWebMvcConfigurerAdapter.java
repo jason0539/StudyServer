4ab0cf4815bf9dda671a50a5d376e83bf985c656
jason0539 <jason0539@163.com>
2018-11-08 15:06:55 +0800

    使用映射配置页面访问，可以不写Controller

--- src/main/java/com/daoming/web/controller/MyWebMvcConfigurerAdapter.java
+++ src/main/java/com/daoming/web/controller/MyWebMvcConfigurerAdapter.java
@@ -2,6 +2,7 @@ package com.daoming.web.controller;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -15,5 +16,15 @@ public class MyWebMvcConfigurerAdapter implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/my/**").addResourceLocations("classpath:/my/");
+    }
+
+    /**
+     * 以前要访问一个页面需要先创建个Controller控制类，再写方法跳转到页面
+     * 在这里配置后就不需要那么麻烦了，直接访问http://localhost:8080/toLogin就跳转到login.htm页面了
+     * @param registry
+     */
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/toReact").setViewName("indexPageByReact.html");
     }
 }
