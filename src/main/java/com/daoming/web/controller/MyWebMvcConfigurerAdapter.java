b3d96977019249b9c416863dd3e8bb8f6b90a385
jason0539 <jason0539@163.com>
2018-11-08 15:03:20 +0800

    映射静态资源路径

---
+++ src/main/java/com/daoming/web/controller/MyWebMvcConfigurerAdapter.java
@@ -0,0 +1,19 @@
+package com.daoming.web.controller;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MyWebMvcConfigurerAdapter implements WebMvcConfigurer {
+
+    /**
+     * 配置静态访问资源
+     *
+     * @param registry
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/my/**").addResourceLocations("classpath:/my/");
+    }
+}
