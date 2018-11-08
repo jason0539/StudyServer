6782e59dbcfa435c88612dccea788faf0fd5643c
jason0539 <jason0539@163.com>
2018-11-08 15:10:51 +0800

    注释

--- src/main/java/com/daoming/web/controller/MyWebMvcConfigurerAdapter.java
+++ src/main/java/com/daoming/web/controller/MyWebMvcConfigurerAdapter.java
@@ -19,8 +19,9 @@ public class MyWebMvcConfigurerAdapter implements WebMvcConfigurer {
     }
 
     /**
-     * 以前要访问一个页面需要先创建个Controller控制类，再写方法跳转到页面
-     * 在这里配置后就不需要那么麻烦了，直接访问http://localhost:8080/toLogin就跳转到login.htm页面了
+     * 以前要访问一个页面需要先创建个Controller控制类，再写方法跳转到页面，比如LearnResourceController中配置好的访问http:localhost:8080/learn/react页面
+     * 在这里配置后就不需要那么麻烦了，直接访问http://localhost:8080/toReact就跳转到indexPageByReact.html页面了，不过这里映射过去的没有带数据而已
+     *
      * @param registry
      */
     @Override
