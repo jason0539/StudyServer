901b9375af40cc6de5184d0bcca389043f7b78f5
jason0539 <jason0539@163.com>
2018-11-08 15:19:29 +0800

    新启一个页面作为注册demo

--- src/main/java/com/daoming/web/controller/MyWebMvcConfigurerAdapter.java
+++ src/main/java/com/daoming/web/controller/MyWebMvcConfigurerAdapter.java
@@ -20,12 +20,13 @@ public class MyWebMvcConfigurerAdapter implements WebMvcConfigurer {
 
     /**
      * 以前要访问一个页面需要先创建个Controller控制类，再写方法跳转到页面，比如LearnResourceController中配置好的访问http:localhost:8080/learn/react页面
-     * 在这里配置后就不需要那么麻烦了，直接访问http://localhost:8080/toReact就跳转到indexPageByReact.html页面了，不过这里映射过去的没有带数据而已
+     * 在这里配置后就不需要那么麻烦了，如果在这里配置，直接访问http://localhost:8080/toReact就跳转到indexPageByReact.html页面了，不过这里映射过去的没有带数据而已
+     * 这里配置了一个静态页面，访问http://localhost:8080/testRegisted
      *
      * @param registry
      */
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/toReact").setViewName("indexPageByReact.html");
+        registry.addViewController("/testRegisted").setViewName("mvcConfigurerRegistedPage.html");
     }
 }
