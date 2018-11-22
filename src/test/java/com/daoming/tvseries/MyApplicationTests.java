bed6979aeacd84bfc66d13b71f417f4cdef0be4a
jason0539 <jason0539@163.com>
2018-11-22 17:27:35 +0800

    测试

--- src/test/java/com/daoming/tvseries/MyApplicationTests.java
+++ src/test/java/com/daoming/tvseries/MyApplicationTests.java
@@ -1,16 +1,34 @@
 package com.daoming.tvseries;
 
+import com.daoming.tvseries.controller.TvSeriesController;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MyApplicationTests {
+    private MockMvc tvSeriesControllerMvc;
 
-	@Test
-	public void contextLoads() {
-	}
+    @Before
+    public void setup() {
+        tvSeriesControllerMvc = MockMvcBuilders.standaloneSetup(new TvSeriesController()).build();
+    }
+
+    @Test
+    public void contextLoads() throws Exception {
+        tvSeriesControllerMvc.perform(MockMvcRequestBuilders.get("/tvseries").accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(MockMvcResultHandlers.print())
+                .andReturn();
+    }
 
 }
