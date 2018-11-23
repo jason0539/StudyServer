46bf1a2cb994ea403cc2bef1d3010eeaefbab425
jason0539 <jason0539@163.com>
2018-11-23 10:49:52 +0800

    注释修改

--- src/main/java/com/daoming/web/controller/MyWebMvcConfigurerAdapter.java
+++ src/main/java/com/daoming/web/controller/MyWebMvcConfigurerAdapter.java
@@ -11,7 +11,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class MyWebMvcConfigurerAdapter implements WebMvcConfigurer {
 
     /**
-     * 配置静态访问资源
+     * 静态资源的映射路径
      *
      * @param registry
      */
@@ -21,6 +21,7 @@ public class MyWebMvcConfigurerAdapter implements WebMvcConfigurer {
     }
 
     /**
+     * ViewController的映射路径
      * 以前要访问一个页面需要先创建个Controller控制类，再写方法跳转到页面，比如LearnResourceController中配置好的访问http:localhost:8080/learn/react页面
      * 在这里配置后就不需要那么麻烦了，如果在这里配置，直接访问http://localhost:8080/toReact就跳转到indexPageByReact.html页面了，不过这里映射过去的没有带数据而已
      * 这里配置了一个静态页面，访问http://localhost:8080/testRegisted
