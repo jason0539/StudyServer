6cfc93fc5a8fda2a04cd86b4043448484f7a31c8
jason0539 <jason0539@163.com>
2018-12-07 20:27:35 +0800

    定时任务

--- src/main/java/com/daoming/MyApplication.java
+++ src/main/java/com/daoming/MyApplication.java
@@ -6,10 +6,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import javax.sql.DataSource;
 
 @SpringBootApplication
+@EnableScheduling
 public class MyApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/daoming/utils/SchedulerTask.java
@@ -0,0 +1,14 @@
+package com.daoming.utils;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SchedulerTask {
+    private int count = 0;
+
+    @Scheduled(cron = "*/6 * * * * ?")
+    private void process() {
+        LogUtils.d("this is scheduler task runing  " + (count++));
+    }
+}
