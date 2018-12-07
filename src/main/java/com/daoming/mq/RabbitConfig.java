fa4be87beedb8070a4b0339090cddad96ec0e9ac
jason0539 <jason0539@163.com>
2018-12-07 19:19:11 +0800

    rabbitmq高级用法

--- src/main/java/com/daoming/mq/RabbitConfig.java
+++ src/main/java/com/daoming/mq/RabbitConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitConfig {
+    //更高级的用法参考http://www.ityouknow.com/springboot/2016/11/30/spring-boot-rabbitMQ.html
     public static final String QUEUE_NAME = "queue_of_test";
 
     @Bean
