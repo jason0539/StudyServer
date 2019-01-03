a92cfc847c3026c2137bd13f3353ff8a8bd13fd1
jason0539 <jason0539@163.com>
2019-01-03 16:07:06 +0800

    补充日志

--- src/main/java/com/daoming/web/login/controller/UserInfoController.java
+++ src/main/java/com/daoming/web/login/controller/UserInfoController.java
@@ -1,5 +1,6 @@
 package com.daoming.web.login.controller;
 
+import com.daoming.utils.LogUtils;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,7 @@ public class UserInfoController {
     @RequestMapping("/userList")
     @RequiresPermissions("userInfo:view")//权限管理;
     public String userInfo() {
+        LogUtils.d("userInfo");
         return "userInfo";
     }
 
@@ -27,6 +29,7 @@ public class UserInfoController {
     @RequestMapping("/userAdd")
     @RequiresPermissions("userInfo:add")//权限管理;
     public String userInfoAdd() {
+        LogUtils.d("userInfoAdd");
         return "userInfoAdd";
     }
 
@@ -38,6 +41,7 @@ public class UserInfoController {
     @RequestMapping("/userDel")
     @RequiresPermissions("userInfo:del")//权限管理;
     public String userDel() {
+        LogUtils.d("userDel");
         return "userInfoDel";
     }
 
