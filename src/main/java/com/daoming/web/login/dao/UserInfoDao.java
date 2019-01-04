12c2f52b5ad291139e88b49da61d8a154fbe5092
jason0539 <jason0539@163.com>
2019-01-04 15:01:19 +0800

    jpa查删操作，删除需要注解

--- src/main/java/com/daoming/web/login/dao/UserInfoDao.java
+++ src/main/java/com/daoming/web/login/dao/UserInfoDao.java
@@ -1,8 +1,15 @@
 package com.daoming.web.login.dao;
 
 import com.daoming.web.login.entity.UserInfo;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 
-public interface UserInfoDao extends CrudRepository<UserInfo, Long> {
+import javax.transaction.Transactional;
+
+public interface UserInfoDao extends JpaRepository<UserInfo, Long> {
     UserInfo findByUsername(String username);
+
+    @Modifying
+    @Transactional
+    Long deleteByUsername(String username);
 }
